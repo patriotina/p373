dd14fafdba1e527b6a396e81b182a3c6a01c82e6
Patriot <patriotina@gmail.com>
2018-05-04 17:19:49 +0400

    create user view and html template

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Names
 
 # Create your views here.
+def names_list(request):
+    names = Names.objects.order_by('second_name')
+    print(names)
+    return render(request, 'contacts/names_list.html', {'names': names})
