67c38171b2ba83284228994e3b10e6fca639e926
Patriot <patriotina@gmail.com>
2018-05-04 11:56:08 +0400

    db works. admin can add data to names model

--- contacts/models.py
+++ contacts/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-#from django.utils import timezone
+from django.utils import timezone
 
 # Create your models here.
 
 class Names(models.Model):
-    id = models.Index()
+    #id = models.Index()
     first_name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=80)
-    #created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
 
     def publish(self):
         seflf.created_date = timezone.now()
         self.save()
 
     def __str__(self):
-        return self.first_name + self.second_name
+        return self.second_name + ' ' + self.first_name
