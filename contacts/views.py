0a6e9c2dc1645274cba8bba77c01dbc8f0718d95
Patriot <patriotina@gmail.com>
2018-05-27 21:22:17 +0400

    several models unsuccessfull tryings

--- contacts/views.py
+++ contacts/views.py
@@ -3,6 +3,10 @@ from .models import Names, ContactType, Contacts
 
 # Create your views here.
 def names_list(request):
-    names = Names.objects.all().order_by('second_name')
+    names = Names.objects.filter().order_by('second_name')
+    #names = Names.objects.prefetch_related("persona_name")
+    #contacts = Contacts.objects.prefetch_related("persona_name")
+
     print(names)
+    #print(contacts)
     return render(request, 'contacts/names_list.html', {'names': names})
