fc08709d412ebefee147c97e226121e703f096b0
Patriot <patriotina@gmail.com>
2018-12-28 10:44:45 +0400

    Add departments and roles to person information. Both of them are separate classes in models.

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Names, Contacts, ContactType
+from .models import *
 
 # Register your models here.
 admin.site.register(Names)
 admin.site.register(Contacts)
 admin.site.register(ContactType)
+admin.site.register(Department)
+admin.site.register(Role)
+
--- contacts/models.py
+++ contacts/models.py
@@ -3,6 +3,21 @@ from django.utils import timezone
 
 # Create your models here.
 
+
+class Department(models.Model):
+    dep_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.dep_name
+
+
+class Role(models.Model):
+    role_name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.role_name
+
+
 class Names(models.Model):
     first_name = models.CharField(max_length=50)
     second_name = models.CharField(max_length=80)
@@ -10,6 +25,8 @@ class Names(models.Model):
     work_phone = models.CharField(max_length=10)
     email = models.EmailField(max_length=50)
     created_date = models.DateTimeField(default=timezone.now)
+    persona_dep = models.ForeignKey(Department, on_delete=models.DO_NOTHING)
+    persona_role = models.ForeignKey(Role, on_delete=models.DO_NOTHING)
 
     def publish(self):
         seflf.created_date = timezone.now()
@@ -17,6 +34,7 @@ class Names(models.Model):
 
     def __str__(self):
         return self.second_name + ' ' + self.first_name
+
 
 class ContactType(models.Model):
     type_description = models.CharField(max_length=20)
@@ -29,6 +47,7 @@ class Contacts(models.Model):
     contact_type = models.ForeignKey(ContactType, on_delete=models.DO_NOTHING)
     contact_data = models.CharField(max_length=200)
     persona_name = models.ForeignKey(Names, on_delete=models.DO_NOTHING)
+
 
     def __str__(self):
         return self.contact_data
