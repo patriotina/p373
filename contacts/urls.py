e355a12e7d0e667c6bb208b3c1216df8ab436f75
Patriot <patriotina@gmail.com>
2018-12-28 16:03:10 +0400

    make output by departments

--- contacts/urls.py
+++ contacts/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.names_list, name='names_list'),
+    url(r'^department(?P<pk>\d+)/$', views.department_list, name='departments')
 ]
--- contacts/views.py
+++ contacts/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from .models import Names, ContactType, Contacts
+from .models import Names, ContactType, Contacts, Department
 
 # Create your views here.
 def names_list(request):
     names = Names.objects.filter().order_by('second_name')
+    deps = Department.objects.filter().order_by('dep_name')
     #names = Names.objects.prefetch_related("persona_name")
     #contacts = Contacts.objects.prefetch_related("persona_name")
 
-    print(names)
+#    print(names)
     #print(contacts)
-    return render(request, 'contacts/names_list.html', {'names': names})
+    return render(request, 'contacts/names_list.html', {'names': names, 'deps': deps})
+
+
+def department_list(request, pk):
+    names = Names.objects.filter(persona_dep=pk)
+    #names = Names.objects.filter().order_by('second_name')
+    return render(request, 'contacts/department.html', {'names': names})
+
